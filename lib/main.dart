ef6ac022798266860e558fb61f9031b8bb9b8765
Berke Atahan <berkecibir@gmail.com>
2025-08-12 15:25:51 +0300

    added providers files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:recipe_app_flutter/feat/core/init/app_init.dart';
+import 'package:recipe_app_flutter/feat/core/routes/app_routes.dart';
 import 'package:recipe_app_flutter/feat/core/widgets/navigation_helper/navigation_helper.dart';
+import 'package:recipe_app_flutter/feat/providers/bloc_provider_set_up.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,9 +15,14 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     AppInit.initDeviceSize(context);
-    return MaterialApp(
-      title: 'Recipe App',
-      navigatorKey: Navigation.navigationKey,
+    return MultiBlocProvider(
+      providers: BlocProvidersSetUp.providers,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Recipe App',
+        routes: AppRoutes.routes,
+        navigatorKey: Navigation.navigationKey,
+      ),
     );
   }
 }
